7f5e9c423b1323c24231995e94938ac7d85cefc2
nohy6630 <nohy6630@gmail.com>
2023-12-19 20:33:13 +0900

    feat: interp 추가

---
+++ interp/a.asm
@@ -0,0 +1,41 @@
+	      INT    0, 36
+	      SUP    0, main
+	      RET    0, 0
+main:
+	      INT    0, 20
+	      LDA    1, 12
+	     LITI    0, 1067
+	      STX    0, 1
+	      POP    0, 1
+	      LDA    1, 16
+	     LITI    0, 1235346
+	      STX    0, 1
+	      POP    0, 1
+	      INT    0, 12
+	      LDA    0, 12
+	      LOD    1, 12
+	      POP    0, 5
+	     ADDR    0, printf
+	      CAL    0, 0
+	      INT    0, 12
+	      LDA    0, 20
+	      LOD    1, 16
+	      POP    0, 5
+	     ADDR    0, printf
+	      CAL    0, 0
+	      INT    0, 12
+	      LDA    0, 28
+	      LOD    1, 12
+	      LOD    1, 16
+	     ADDI    0, 0
+	      POP    0, 5
+	     ADDR    0, printf
+	      CAL    0, 0
+	      LDA    1, -4
+	     LITI    0, 0
+	      STO    0, 1
+	      RET    0, 0
+	      RET    0, 0
+.literal    12 "%d\n"
+.literal    20 "%d\n"
+.literal    28 "%d\n"
